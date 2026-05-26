a4c8e4fe8778f0001365a93c841812debea4e728
raghav(raghava-0650) <raghav83286@gmail.com>
2026-05-26 10:06:34 +0530

    generate and display testcases

---
+++ app/api/test-cases/settings/route.ts
@@ -0,0 +1,23 @@
+import { eq } from 'drizzle-orm';
+import {
+  NextRequest,
+  NextResponse,
+} from 'next/server';
+
+import { db } from '@/db';
+import { TestCasesTable } from '@/db/schema';
+
+export async function POST(req: NextRequest) {
+    const { title, description, targetRoute, expectedResult, testCaseId } = await req.json();
+
+    const result = await db.update(TestCasesTable).set({
+        title,
+        description,
+        targetRoute,
+        expectedResult
+    }).where(eq(TestCasesTable.id, testCaseId)).returning();
+
+
+    return NextResponse.json(result[0]);
+
+}
